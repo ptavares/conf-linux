e1096d428c4e8dc8a9a1c741fb23a4cd0d5ea750
ptavares <ptavares@ippon.fr>
2017-10-31 16:55:36 +0100

    config fish update

--- config.fish
+++ config.fish
@@ -3,7 +3,15 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
+# functions
+function sync_history --on-event fish_preexec
+    history --save
+    history --merge
+end
+
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
 set PATH ~/cargo/bin $PATH
+
+
