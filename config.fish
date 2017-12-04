bebc2fa2b81fe722db0f98ccc63a64977b70c34c
Patrick Tavares <ptavares@ippon.fr>
2017-12-04 11:14:33 +0100

    add function to change window title in tmux

--- config.fish
+++ config.fish
@@ -21,3 +21,24 @@ set PATH /usr/local/go/bin $PATH
 export GOPATH=$HOME/projects/go
 
 source $HOME/.config/fish/aliases.fish
+
+# Set the tmux window title, depending on whether we are running something, or just prompting
+
+function fish_title
+	if [ "fish" != $_ ]
+		tmux rename-window "$argv"
+	else
+        tmux_directory_title
+	end
+end
+
+function tmux_directory_title
+	if [ "$PWD" != "$LPWD" ]
+		set LPWD "$PWD"
+		set INPUT $PWD
+		set SUBSTRING (eval echo $INPUT| awk '{ print substr( $0, length($0) - 19, length($0) ) }')
+		tmux rename-window "..$SUBSTRING"
+	end
+end
+
+
