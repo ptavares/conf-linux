7dd5ec274d0a6275f464b27b69009ad6db714a3f
Patrick Tavares <ptavares@ippon.fr>
2018-01-02 10:47:56 +0100

    change size from 730mo to 4,7go for backup

--- utils/backupPC.sh
+++ utils/backupPC.sh
@@ -3,12 +3,12 @@ KEYBASE_PRIVATE_DIR=/keybase/private/${USER}
 
 function full {
     echo "Make a full bakcup..."
-    dar -v -c "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/full_backup" -R "${HOME}" -w -s 734003200 -y -P Musique -P Téléchargements -P Vidéos -P VirtualBoxVMs -X
+    dar -v -c "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/full_backup" -R "${HOME}" -w -s 4734003200 -y -P Musique -P Téléchargements -P Vidéos -P VirtualBoxVMs -X
 }
 
 function diff {
     echo "Make a diff bakcup..."
-    dar -v -c  "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/diff_backup_`date -I`" -R "${HOME}" -A "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/full_backup" -w -s 734003200 -y -P Musique -P Téléchargements -P Vidéos -P VirtualBoxVMs -X
+    dar -v -c  "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/diff_backup_`date -I`" -R "${HOME}" -A "${KEYBASE_PRIVATE_DIR}/00_BAKCUP/full_backup" -w -s 4734003200 -y -P Musique -P Téléchargements -P Vidéos -P VirtualBoxVMs -X
 }
 
 
